0f8830f092f7ad2719bafb97ed58c9ba7f8933c1
Dannae <danavalegg15@gmail.com>
2025-05-17 12:53:54 -0500

    se elimino la importacion de hasMany ya que no se estaba llamando en la clase

--- app/models/proyectos.ts
+++ app/models/proyectos.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, belongsTo, hasMany } from '@adonisjs/lucid/orm'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
 import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 import Cliente from '../models/clientes.js'
 
